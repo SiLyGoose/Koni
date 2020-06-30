4709f01583b3cc3e4902d213ffcdcc171b44b1c0
aznballer420 <zzisandstormzz9@gmail.com>
2020-06-29 20:39:47 -0700

    code placement err

--- events/client/ready.js
+++ events/client/ready.js
@@ -12,17 +12,20 @@ module.exports = async bot => {
             if (settings.notifs && settings.channelID) {
                 let channel = bot.channels.cache.get(settings.channelID.match(/\d{18}/g)[0]);
 
-                let playerNames = settings.userList;
-                let names = [];
-    
-                for (let i = 0; i < res.samplePlayers.length; i++) {
-                    let playerName = res.samplePlayers[i].name;
-                    let filter = playerNames.filter(x => { return x.ign === playerName })
-                    names.push(filter.irl || playerName);
-                }
-
                 return await ping(settings.ipaddr, 25565, (err, res) => {
+                    if (err) return;
                     Embed.fields = [], Embed.description = null, Embed.thumbnail = null;
+
+
+                    let playerNames = settings.userList;
+                    let names = [];
+
+                    for (let i = 0; i < res.samplePlayers.length; i++) {
+                        let playerName = res.samplePlayers[i].name;
+                        let filter = playerNames.filter(x => { return x.ign === playerName })
+                        names.push(filter.irl || playerName);
+                    }
+
                     Embed.setThumbnail(err || res.favicon.length > 2048 ? "https://lh3.googleusercontent.com/VSwHQjcAttxsLE47RuS4PqpC4LT7lCoSjE7Hx5AW_yCxtDvcnsHHvm5CTuL5BPN-uRTP" : res.favicon)
                         .setTitle(err ? "Server Down" : `${res.host}:${res.port}`)
                         .addField(`Server Status`, `\`\`\`ini\n[Version]\n${err ? settings.lastKnownVersion : res.version}\n[MOTD]\n${err ? "Unavailable" : res.descriptionText}\`\`\``)
